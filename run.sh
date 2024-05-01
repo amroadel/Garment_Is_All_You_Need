94955cc15e56ff5c83ad19406cf51dcc1dab7bf5
amroadel <amroadel@aucegypt.edu>
2024-05-01 04:53:03 -0400

    fixed run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the paths
-MODEL_PATH="../results/body_images"
+MODEL_PATH="../../results/body_images/body_0.png"
 CLOTH_PATH="garment.jpg"
 SCALE=2.0
 SAMPLE=2
@@ -15,12 +15,29 @@ garment_types[2]="dress"
 
 GARMENT_TYPE="${garment_types[$CATEGORY]}"
 
-python run.py --garment_path "$CLOTH_PATH" --garment_type "$GARMENT_TYPE"
+python run.py --garment_path "$CLOTH_PATH" --garment_type "$GARMENT_TYPE" 
 
 cd OOTDiffusion/run
 IMAGES_FOLDER="../../results/body_images"
 
 # Loop through each image file in the folder
-for IMAGE_PATH in "$IMAGES_FOLDER"/body*.png; do
-    python run_ootd.py --model_path "$IMAGE_PATH" --cloth_path "../../$CLOTH_PATH" --scale "$SCALE" --sample "$SAMPLE"  --model_type "$MODEL_TYPE" --category $CATEGORY
-done
+# for IMAGE_PATH in "$IMAGES_FOLDER"/body*.png; do
+#     rm -r images_output/*
+#     python run_ootd.py --model_path "$IMAGE_PATH" --cloth_path "../../$CLOTH_PATH" --scale "$SCALE" --sample "$SAMPLE"  --model_type "$MODEL_TYPE" --category $CATEGORY
+#     mkdir -p ../../results/first
+#     mv -r images_output/ ../../results/first
+# done
+
+rm -r images_output/*
+MODEL_PATH="../../results/body_images/body_0.png"
+python run_ootd.py --model_path "$MODEL_PATH" --cloth_path "../../$CLOTH_PATH" --scale "$SCALE" --sample "$SAMPLE"  --model_type "$MODEL_TYPE" --category $CATEGORY
+mkdir -p ../../results/first
+mv images_output/* ../../results/first/
+
+rm -r images_output/*
+MODEL_PATH="../../results/body_images/body_1.png"
+python run_ootd.py --model_path "$MODEL_PATH" --cloth_path "../../$CLOTH_PATH" --scale "$SCALE" --sample "$SAMPLE"  --model_type "$MODEL_TYPE" --category $CATEGORY
+mkdir -p ../../results/second
+mv images_output/* ../../results/second/
+
+# python run_ootd.py --model_path "$MODEL_PATH" --cloth_path "../../$CLOTH_PATH" --scale "$SCALE" --sample "$SAMPLE"  --model_type "$MODEL_TYPE" --category $CATEGORY
